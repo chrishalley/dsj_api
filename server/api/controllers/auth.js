e9a2d0958fdf702904264212fd46dc9c3cc6f744
Chris Halley <chrishalley86@gmail.com>
2018-12-03 22:29:38 +1000

    Split auth route logic into controlller

---
+++ server/api/controllers/auth.js
@@ -0,0 +1,19 @@
+const User = require('../../models/user');
+
+exports.auth_login = (req, res, next) => {
+  const credentials = req.body;
+  User.findByEmail(credentials.email)
+    .then(user => {
+      return user.checkPassword(credentials.password)
+    })
+    .then(user => {
+      return user.generateAuthToken();
+    })
+    .then(user => {
+      res.status(200).send(user);
+    })
+    .catch(e => {
+      console.log(e);
+      res.status(e.status).send(e);
+    });
+};
--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -1,27 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../../models/user');
+const AuthController = require('../controllers/auth');
 
 // USER LOGIN
-router.post('/login', (req, res, next) => {
-  const credentials = req.body;
-  User.findByEmail(credentials.email)
-    .then(user => {
-      return user.checkPassword(credentials.password)
-    })
-    .then(user => {
-      return user.generateAuthToken();
-    })
-    .then(user => {
-      res.status(200).send(user);
-    })
-    .catch(e => {
-      console.log(e);
-      res.status(e.status).send(e);
-    });
-});
-
-
+router.post('/login', AuthController.auth_login);
 
 module.exports = router;
