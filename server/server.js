c00c29a261b636d0410be0ab066e0c40a57c7471
Chris Halley <chrishalley86@gmail.com>
2018-12-10 09:51:02 +1300

    removed redundant console.logs

--- server/server.js
+++ server/server.js
@@ -9,15 +9,6 @@ const applicationError = require('./errors/applicationErrors');
 const app = express();
 app.use(bodyParser.json());
 
-const usersRoutes = require('./api/routes/users');
-const authRoutes = require('./api/routes/auth');
-
-app.use(morgan('dev'));
-app.use('/users', usersRoutes);
-app.use('/auth', authRoutes);
-
-const port = process.env.PORT;
-
 // CORS Options Config
 if (process.env.NODE_ENV !== 'production') {
   var corsOptions = {
@@ -32,7 +23,16 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.use(cors(corsOptions));
-const {User} = require('./models/user');
+
+const usersRoutes = require('./api/routes/users');
+const authRoutes = require('./api/routes/auth');
+
+app.use(morgan('dev'));
+app.use('/users', usersRoutes);
+app.use('/auth', authRoutes);
+
+const port = process.env.PORT;
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
@@ -77,15 +77,12 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   let err = new applicationError.GeneralError();
   if (error instanceof applicationError.ApplicationError) {
-    // console.log('applicationError: ', error);
     res.status(error.status).send(error);
   } else if (error.name === "MongoError") {
-    // console.log('MongoError: ', error);
     err.message = error.errmsg;
     err.code = error.code;
     res.status(err.status).send(err);
   } else {
-    // console.log('ERROR***: ', JSON.stringify(error, null, 2));
     const customError = new applicationError.GeneralError();
     res.status(error.status).send(error);
   }
